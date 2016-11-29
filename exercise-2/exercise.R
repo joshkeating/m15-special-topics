74f6cce47502c8972412a896505c38ec7de8a014
Josh Keating <josh.keating7@gmail.com>
2016-11-29 14:44:17 -0800

    machine learning

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,20 +1,15 @@
-# Introductory example using the housing data used here: 
-# http://www.r2d3.us/visual-intro-to-machine-learning-part-1/
 library(rpart)
 library(rpart.plot)
 
 # Read in data
-setwd('~/Documents/info-201/m15-special-topics/exercise-2')
+setwd('~/School_16-17/Info-201/bin/m15-special-topics/exercise-2/')
 homes <- read.csv('data/housing-data.csv')
 
-# Function to compare values
 AssessFit <- function(model, data = homes, outcome = 'in_sf') {
   predicted <- predict(model, data, type='class')
   accuracy <- length(which(data[,outcome] == predicted)) / length(predicted) * 100
   return(accuracy)
 }
-
-# Assess fit for different models
 
 # Use rpart to fit a model: predict `in_sf` using all other variables
 basic.fit <- rpart(in_sf ~ ., data = homes, method="class")
@@ -32,10 +27,12 @@ for(i in 1:100) {
   # Create test and training data
   # Hint: http://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
   # 1. Create training and testing datasets by sampling 75% of your data from your `homes` dataframe.
-  
+  train.indicies <- sample(seq_len(nrow(homes)), size = sample.size)
+  training.data <- homes[train.indicies,]
+  test.data <- homes[-train.indicies,]
   
   # 2. Pass your **training data** to the `rpart` function to run a simple classification operation
-  
+  basic.fit <- rpart(in_sf ~ ., data = training.data, method="class")
   
   # 3. Pass your results to the `AssessFit` function to assess the fit
   
