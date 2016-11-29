e552d233847844cc216555cdd1c1c4523f020aab
Josh Keating <josh.keating7@gmail.com>
2016-11-29 14:39:47 -0800

    switching to master

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,7 +4,7 @@ library(rpart)
 library(rpart.plot)
 
 # Read in data
-setwd('~/Documents/info-201/m15-special-topics/exercise-2')
+setwd('~/School_16-17/Info-201/bin/m15-special-topics/exercise-2/')
 homes <- read.csv('data/housing-data.csv')
 
 # Function to compare values
